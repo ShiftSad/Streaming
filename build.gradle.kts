3abddffa5fc8d904d18a0adf0a643a518b96d967
ShiftSad <rosalimavictor@gmail.com>
2024-08-04 12:51:01 -0300

    ShadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("io.github.goooler.shadow") version "8.1.7"
     id("java")
 }
 
@@ -8,4 +9,10 @@ repositories {
 
 dependencies {
     implementation("net.minestom:minestom-snapshots:7b180172ce")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "codes.shiftmc.streaming.Server"
+    }
 }
